3775119eb2d3fa30e223a4154bd53ae2a2566191
Rodrigo de Santiago <r.desantiago@globant.com>
2020-08-16 21:08:17 -0300

    Create project

---
+++ UruguayCampingGuide/UruguayCampingGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UruguayCampingGuide
+//
+//  Created by Rodrigo de Santiago on 8/16/20.
+//  Copyright © 2020 Rodrigo de Santiago. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
